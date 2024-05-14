a809e4c2d826a331dc5e98fe24c34bdf78228a9a
GiseEveRam <evelynmdp@hotmail.com>
2024-05-14 08:45:16 -0300

    cambie el index

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
-import express from 'express';
+import express, { json } from "express";
 import fs from "fs";
+//MIDELWARE
 import bodyParser from "body-parser";
 
 const app = express();
 app.use(bodyParser.json());
 
 const readData = () => {
-    try {
-    const data = fs.readFileSync("./db.json");
-    return JSON.parse(data);
+    try{
+        const data = fs.readFileSync("./db.json");
+        return JSON.parse(data);
 
-    } catch (error){
-        console.log(error)
+    } catch (error) {
+        console.log(error);
 
     }
     
+
 };
 
-const writeData = (data) =>
-{
+const writeData = (data) => {
     try{
-    fs.writeFileSync("./db.json", JSON.stringify(data));
-    }
-    catch(error){
-        console.log(error)
+        fs.writeFileSync("./db.json", JSON.stringify(data));
+
+    } catch (error) {
+        console.log(error);
 
     }
-}
-app.get("/",(req, res) =>{
-    res.send("Welcome to My Api");
+
+};
+
+app.get("/", (req, res) => {
+    res.send("Welcome to my Api!!!");
+
 });
 
-
-app.get("/peliculas", (req, res) =>{
+//ENDOPOINTS PARA LA DB
+app.get("/peliculas", (req, res) => {
     const data = readData();
     res.json(data.peliculas);
+
 });
 
 app.get("/peliculas/:id", (req, res) => {
@@ -42,46 +47,48 @@ app.get("/peliculas/:id", (req, res) => {
     const id = parseInt(req.params.id);
     const pelicula = data.peliculas.find((pelicula) => pelicula.id === id);
     res.json(pelicula);
-   
+
 });
+
+//OBTENER
 
 app.post("/peliculas", (req, res) => {
     const data = readData();
     const body = req.body;
     const newPelicula = {
-        id: data.peliculas.length +1,
+        id: data.peliculas.legth + 1,
         ...body,
     };
+
     data.peliculas.push(newPelicula);
     writeData(data);
     res.json(newPelicula);
+
 });
 
-app.put("/peliculas/:id", (req,res) =>{
+app.put("/peliculas/:id", (req, res) => {
     const data = readData();
     const body = req.body;
     const id = parseInt(req.params.id);
-    const peliculaIndex = data.peliculas.findIndex((pelicula) => pelicula.id ===id);
+    const peliculaIndex = data.peliculas.findIndex((pelicula) => pelicula.id === id);
     data.peliculas[peliculaIndex] = {
         ...data.peliculas[peliculaIndex],
         ...body,
     };
     writeData(data);
-    res.json({ message: "Peliculas updated sucessfully"});
+    res.json({ message : "Pelicula modificada"});
 });
-
-app.delete("peliculas/:id", (req, res) => {
+app.delete("/peliculas/:id", (req, res) => {
     const data = readData();
     const id = parseInt(req.params.id);
-    data.peliculaIndex = data.peliculas.findIndex((pelicula) => pelicula.id === id);
+    const peliculaIndex = data.peliculas.findIndex((pelicula) => pelicula.id === id);
     data.peliculas.splice(peliculaIndex, 1);
     writeData(data);
-    res.json({ message : "peliculas deleted sucessfully"});
+    res.json({ message: "peliculas eliminada!!"});
+   
 });
 
-
-
-
-app.listen(3000, () => {
+app.listen(3000, () =>{
     console.log('Servidor corriendo en puerto 3000');
 });
+
